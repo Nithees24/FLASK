78dff6a9f454ad705c0ce2a509842634e92c685c
Nithees24 <nithees2424@gmail.com>
2026-03-24 17:25:01 +0530

    Added files

---
+++ app_flask.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, make_response
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template(index.html)
+
+@app.route('/setcookie', methods = ['GET', 'POST'])
+def setcookie():
+    if request.method == 'POST':
+        user =  request.form['nm']
